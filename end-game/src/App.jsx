8c7ec3f7aaa126aee4332ceafab87d5677de9626
Kartik Marwal <kartikmarwal25@gmail.com>
2025-07-21 01:31:45 +0530

    INITIAL COMMIT

---
+++ end-game/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useState } from 'react'
+import Header from './components/header.jsx'
+// import Main from './components/main.jsx'  
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+    <Header />
+    {/* <Main /> */}
+    </>
+  )
+}
+
+export default App
